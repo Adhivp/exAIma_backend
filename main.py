1f47e52591680219871801f6977b995c4e6a68e1
Adhivp <adhivp910@gmail.com>
2025-03-16 06:43:37 +0530

    Enhance server performance by adding uvloop support and increasing worker count in uvicorn configuration; update requirements to include uvloop

--- main.py
+++ main.py
@@ -96,4 +96,11 @@ app.include_router(public_router)
 app.include_router(protected_router, prefix="/api")
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    import uvloop
+    uvicorn.run(
+        "main:app", 
+        host="0.0.0.0", 
+        port=8000, 
+        workers=8, 
+        loop="uvloop"
+    )
